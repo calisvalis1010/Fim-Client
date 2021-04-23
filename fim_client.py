d15f6382ead7c7599dbdc98c10b56c2dbac3396d
ClickyDev <63604522+calisvalis1010@users.noreply.github.com>
2021-04-23 17:57:07 +0300

    this isnt a version

---
+++ fim_client.py
@@ -0,0 +1,14 @@
+import os
+
+
+print(' 000000      0           00       0')
+print(' 0                       0 0     00')
+print(' 000000      0           0  0   0 0')
+print(' 0           0           0   000  0')
+print(' 0           0           0        0')
+print(' 0           0           0        0')
+print(' 0           0           0        0')
+print(' 0           0           0        0')
+print('                          client               ')
+print('             ')           
+os.system('pause')
