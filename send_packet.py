9a4e28eb421abaaf72c6d42cd14e950d8076cb5f
ClickyDev <63604522+calisvalis1010@users.noreply.github.com>
2021-04-27 10:04:18 +0300

    Update send_packet.py

--- send_packet.py
+++ send_packet.py
@@ -8,7 +8,7 @@ f.close()
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 try:
     s.connect((str(ip), 19132))
-    s.send(str(sys.argv[0]+' ' + sys.argv[1]+' ' + sys.argv[2]+' ' + sys.argv[3+' ' + sys.argv[4]+' ' + sys.argv[5]).encode("utf-8"))
+    s.send(str(sys.argv[0]+' ' + sys.argv[1]+' ' + sys.argv[2]+' ' + sys.argv[3]+' ' + sys.argv[4]+' ' + sys.argv[5]).encode("utf-8"))
     s.close()     
 except KeyboardInterrupt:
     sys.exit(0)
